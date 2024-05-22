73dd62025f21815080cb7ce4df9477d530beb289
Shino <sakifhussain33@gmail.com>
2024-05-22 18:49:24 +0600

    Theme stuffs

--- wunderbuy/lib/utils/themes/theme.dart
+++ wunderbuy/lib/utils/themes/theme.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
+import "package:wunderbuy/utils/themes/custom_themes/text_theme.dart";
 
-class CAppTheme{ //C as in custom classes
+class CAppTheme {
+  //C as in custom classes
   CAppTheme._();
 
   static ThemeData lightTheme = ThemeData(
@@ -9,8 +11,15 @@ class CAppTheme{ //C as in custom classes
     brightness: Brightness.light,
     primaryColor: Colors.green[400],
     scaffoldBackgroundColor: Colors.white,
-
+    textTheme: CTextTheme.lightTextTheme,
+    elevatedButtonTheme: ElevatedButtonThemeData(),
   );
-  static ThemeData darkTheme = ThemeData();
-
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.green[400],
+    scaffoldBackgroundColor: Colors.black54,
+    textTheme: CTextTheme.darkTextTheme,
+  );
 }
