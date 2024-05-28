34a739fa726322f0da8a2ff9d0fd5a436ec426f1
Shino <sakifhussain33@gmail.com>
2024-05-28 17:26:59 +0600

    constant and first batch of icons

---
+++ wunderbuy/lib/utils/constants/image_strings.dart
@@ -0,0 +1,12 @@
+class CImages {
+  CImages._();
+  //app
+  static const String darkAppLogo = "assets/logos/png/logo-white.png";
+  static const String lightAppLogo = "assets/logos/png/logo-black.png";
+  static const String mainAppLogo = "assets/logos/png/logo-no-background.png";
+
+  //Icons
+  static const String iconGoogle = "assets/icons/google_icon.png";
+  static const String iconGoogleDark = "assets/icons/google_dark_icon.png";
+  static const String iconFacebook = "assets/icons/facebook_icon.png";
+}
---
+++ wunderbuy/lib/utils/constants/sizes.dart
@@ -0,0 +1,17 @@
+class CSizes {
+  CSizes._();
+  //padding and margins
+  static const double xs = 4.0;
+
+  //Icon sizes
+  static const double iconXs = 12.0;
+  static const double iconSm = 16.0;
+  static const double iconM = 32.0;
+  static const double iconL = 32.0;
+  //font sizes
+  static const double fontSm = 14.0;
+  static const double fontM = 16.0;
+  static const double fontL = 18.0;
+  //button sizes
+  
+}
