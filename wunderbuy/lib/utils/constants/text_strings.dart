804a852cc3d821ba192a7de3bdcf551a4aacb468
Shino <sakifhussain33@gmail.com>
2024-05-28 18:26:30 +0600

    updated text strings

---
+++ wunderbuy/lib/utils/constants/text_strings.dart
@@ -0,0 +1,12 @@
+class CTextStrings{
+  CTextStrings._();
+  static const String onBoardingTitle_1 = "Choose!";
+  static const String onBoardingTitle_2 = "Pay with Convenience!";
+  static const String onBoardingTitle_3 = "Recieve at your doorstep!";
+
+  static const String onBoardingSubTitle_1 = "Welcome to a world of limitless choice! Your next favorite product awaits!";
+  static const String onBoardingSubTitle_2 = "For seamless transaction, select your payment method in the account settings";
+  static const String onBoardingSubTitle_3 = "Your Convenience, our priority";
+
+  static const String homeAppBarTitle = "Happy Shopping!";
+}
