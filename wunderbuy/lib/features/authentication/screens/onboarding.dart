4c0f718a2e052762b937a5670f97ace8f197c500
Shino <sakifhussain33@gmail.com>
2024-06-10 15:07:24 +0600

    on boarding screen

--- wunderbuy/lib/features/authentication/screens/onboarding.dart
+++ wunderbuy/lib/features/authentication/screens/onboarding.dart
@@ -14,33 +14,58 @@ class OnboardingScreen extends StatelessWidget {
       body: Stack(
         children: [
           PageView(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(CSizes.defaultSpace),
-                child: Column(
-                  children: [
-                    Lottie.asset(
-                     CImages.lottieSearching,
-                     width: CHelperFunctions.sceenWidth() * 0.8,
-                     height: CHelperFunctions.screenHeight() * 0.7,
-                    ),
-                    Text(
-                      CTextStrings.onBoardingTitle_1,
-                      style: Theme.of(context).textTheme.headlineMedium,
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(
-                      height: CSizes.spaceBtwItemsSmall,
-                    ),
-                    Text(
-                      CTextStrings.onBoardingSubTitle_1,
-                      style: Theme.of(context).textTheme.bodyMedium,
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
-              ),
+            children: const [
+              OnBoardingPage(
+                lottie: CImages.lottieSearching, 
+                title: CTextStrings.onBoardingTitle_1, 
+                subtitle: CTextStrings.onBoardingSubTitle_1,),
+              OnBoardingPage(
+                lottie: CImages.lottieShopping, 
+                title: CTextStrings.onBoardingTitle_2, 
+                subtitle: CTextStrings.onBoardingSubTitle_2,),
+              OnBoardingPage(
+                lottie: CImages.lottieDelivery, 
+                title: CTextStrings.onBoardingTitle_3, 
+                subtitle: CTextStrings.onBoardingSubTitle_3,),
             ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({
+    super.key, required this.lottie, required this.title, required this.subtitle,
+  });
+
+  final dynamic lottie;
+  final String title, subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(CSizes.defaultSpace),
+      child: Column(
+        children: [
+          Lottie.asset(
+           lottie,
+           width: CHelperFunctions.sceenWidth() * 0.8,
+           height: CHelperFunctions.screenHeight() * 0.7,
+          ),
+          Text(
+            title,
+            style: Theme.of(context).textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: CSizes.spaceBtwItemsSmall,
+          ),
+          Text(
+            subtitle,
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
           ),
         ],
       ),
